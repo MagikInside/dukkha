9dd13c2781d05ded9799ca7c6676bb7682a11ad3
luisma <luismamoran@gmail.com>
2022-03-21 20:38:25 +0100

    feat(login) add firebase auth with google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
@@ -20,6 +21,7 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatButtonModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth())
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
 import { ELEMENTS } from '../models/elements';
 import { Element } from '../models/element.model'; 
+import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +14,7 @@ export class LoginComponent implements OnInit {
   selectedElement: Element | null = null;
   readonly elements = ELEMENTS;
 
-  constructor() { }
+  constructor(@Optional() private auth: Auth) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +23,10 @@ export class LoginComponent implements OnInit {
     if (element) {
       this.selectedElement = element;
   }
+}
+
+async login() {
+  return await signInWithPopup(this.auth, new GoogleAuthProvider());
 }
 
 }
