8cd4d72df8ec6fc5e88dd5d1bb2822de15fdd180
luisma <luismamoran@gmail.com>
2022-03-30 21:15:41 +0200

    feat(intro) add user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  user$: Observable<firebase.User | null>;
+
+  constructor(private readonly auth: AngularFireAuth) { 
+    this.user$ = this.auth.authState;
+  }
 }
